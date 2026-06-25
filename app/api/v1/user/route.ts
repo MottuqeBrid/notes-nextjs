2163238a939540b6239da57456345996b7e80ece
Mottuqe Brid <mottuqebrid@gmail.com>
2026-06-25 16:12:28 +0600

    test MONGODB_URI

--- app/api/v1/user/route.ts
+++ app/api/v1/user/route.ts
@@ -3,9 +3,19 @@ import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
+    const mongo_url = process.env.MONGODB_URI;
+    if (!mongo_url) {
+      return Response.json(
+        { error: "MONGO_URL is not defined" },
+        { status: 500 },
+      );
+    }
     await connectDB();
-    
+    return Response.json({ message: "Hello World", mongo_url });
   } catch (error) {
-    
+    return Response.json(
+      { error: "Failed to connect to database" },
+      { status: 500 },
+    );
   }
 }
