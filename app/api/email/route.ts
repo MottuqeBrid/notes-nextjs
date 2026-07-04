b60ce54624ae85fc9afaecfa40416c9659f7e764
Mottuqe Brid <mottuqebrid@gmail.com>
2026-07-05 00:28:13 +0600

    feat: enhance email API with CORS headers and consistent response handling

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,10 +1,15 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Email from "@/models/emailModel";
 import "@/models/noteModel";
 import "@/models/userModel";
 import "@/models/fileModel";
 import { connectDB } from "@/lib/mongoose";
 import User from "@/models/userModel";
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*", // Or 'https://example.com'
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function POST(request: NextRequest) {
   let to;
@@ -19,20 +24,30 @@ export async function POST(request: NextRequest) {
       ...body,
     });
 
-    return Response.json({
-      success: true,
-      message: "Email saved successfully",
-      id: email._id,
-    });
+    // return Response.json({
+    //   success: true,
+    //   message: "Email saved successfully",
+    //   id: email._id,
+    // });
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Email saved successfully",
+        id: email._id,
+      },
+      {
+        headers: corsHeaders,
+      },
+    );
   } catch (error) {
-    return Response.json(
+    return NextResponse.json(
       {
         success: false,
         message:
           error instanceof Error ? error.message : "Internal Server Error",
       },
       {
-        status: 500,
+        headers: corsHeaders,
       },
     );
   } finally {
@@ -48,9 +63,14 @@ export async function POST(request: NextRequest) {
 
 export async function GET() {
   const emails = await Email.find().sort({ createdAt: -1 });
-  return Response.json({
-    success: true,
-    message: "GET request received successfully",
-    emails,
-  });
+  return NextResponse.json(
+    {
+      success: true,
+      message: "GET request received successfully",
+      emails,
+    },
+    {
+      headers: corsHeaders,
+    },
+  );
 }
